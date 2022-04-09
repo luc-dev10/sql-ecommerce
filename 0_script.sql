bb5dcae552fa6d2a488f62f8eae5ecedc739483b
luc-dev10 <luc.dev1008@gmail.com>
2022-04-09 13:23:17 -0400

    add category table

--- 0_script.sql
+++ 0_script.sql
@@ -8,18 +8,30 @@ USE `ecommerce` ;
 
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
 
--- create product table
-CREATE TABLE IF NOT EXISTS `ecommerce.product` (
-  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) DEFAULT NULL,
-  `description` VARCHAR(4000) DEFAULT NULL,
-  `price` DECIMAL(13,2) DEFAULT NULL,
-  `quantity` INT(11) DEFAULT NULL,
-  `image_url` VARCHAR(255) DEFAULT NULL,
-  `date_created` DATETIME(6) DEFAULT NULL,
-  `date_updated` DATETIME(6) DEFAULT NULL,
-  PRIMARY KEY(`id`)
+-- create table for category
+DROP TABLE IF EXISTS ecommerce.category;
+CREATE TABLE ecommerce.category (
+	id BIGINT(20) NOT NULL AUTO_INCREMENT,
+    category_name VARCHAR(255) NOT NULL,
+    PRIMARY KEY(id)
 )
-ENGINE=InnoDB
 AUTO_INCREMENT = 1;
 
+-- create product table
+DROP TABLE IF EXISTS ecommerce.product;
+CREATE TABLE ecommerce.product (
+  id BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) DEFAULT NULL,
+  `description` VARCHAR(4000) DEFAULT NULL,
+  price DECIMAL(13,2) DEFAULT NULL,
+  quantity INT(11) DEFAULT NULL,
+  image_url VARCHAR(255) DEFAULT NULL,
+  date_created DATETIME(6) DEFAULT NULL,
+  date_updated DATETIME(6) DEFAULT NULL,
+  category_id BIGINT(20) NOT NULL,
+  PRIMARY KEY(`id`),
+  
+  FOREIGN KEY (category_id) 
+	REFERENCES category(id)
+)
+AUTO_INCREMENT = 1;

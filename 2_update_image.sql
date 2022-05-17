d6ff207368b9f09db23dc83bf38106c178cf84ec
luc-dev10 <luc.dev1008@gmail.com>
2022-05-17 12:14:01 -0400

    update image links to jpg

--- 2_update_image.sql
+++ 2_update_image.sql
@@ -1,94 +1,94 @@
 
 update ecommerce.product
-set image_url = 'assets/product/wrench.jpg'
+set image_url = 'assets/images/product/wrench.jpg'
 where id = 1;
 
 update ecommerce.product
-set image_url = 'assets/product/screwdriver.jpg'
+set image_url = 'assets/images/product/screwdriver.jpg'
 where id = 2;
 
 update ecommerce.product
-set image_url = 'assets/product/hard_hat.jpg'
+set image_url = 'assets/images/product/hard_hat.jpg'
 where id = 3;
 
 update ecommerce.product
-set image_url = 'assets/product/ratchet.webp'
+set image_url = 'assets/images/product/ratchet.jpg'
 where id = 4;
 
 update ecommerce.product
-set image_url = 'assets/product/plier.jpg'
+set image_url = 'assets/images/product/plier.jpg'
 where id = 5;
 
 update ecommerce.product
-set image_url = 'assets/product/drill.jpg'
+set image_url = 'assets/images/product/drill.jpg'
 where id = 6;
 
 update ecommerce.product
-set image_url = 'assets/product/tape.jpg'
+set image_url = 'assets/images/product/tape.jpg'
 where id = 7;
 
 update ecommerce.product
-set image_url = 'assets/product/tape.jpg'
+set image_url = 'assets/images/product/measurement_tape.jpg'
 where id = 8;
 
 update ecommerce.product
-set image_url = 'assets/product/prybar.jpg'
+set image_url = 'assets/images/product/prybar.jpg'
 where id = 9;
 
 update ecommerce.product
-set image_url = 'assets/product/saw.jpg'
+set image_url = 'assets/images/product/saw.jpg'
 where id = 10;
 
 update ecommerce.product
-set image_url = 'assets/product/safety_boots.jpg'
+set image_url = 'assets/images/product/safety_boots.jpg'
 where id = 11;
 
 update ecommerce.product
-set image_url = 'assets/product/gloves.jpg'
+set image_url = 'assets/images/product/gloves.jpg'
 where id = 12;
 
 update ecommerce.product
-set image_url = 'assets/product/parka.png'
+set image_url = 'assets/images/product/parka.png'
 where id = 13;
 
 update ecommerce.product
-set image_url = 'assets/product/coverall.webp'
+set image_url = 'assets/images/product/coverall.jpg'
 where id = 14;
 
 update ecommerce.product
-set image_url = 'assets/product/respirator.png'
+set image_url = 'assets/images/product/respirator.jpg'
 where id = 15;
 
 update ecommerce.product
-set image_url = 'assets/product/ear_muffs.jpg'
+set image_url = 'assets/images/product/ear_muffs.jpg'
 where id = 16;
 
 update ecommerce.product
-set image_url = 'assets/product/broom.jpg'
+set image_url = 'assets/images/product/broom.jpg'
 where id = 17;
 
 update ecommerce.product
-set image_url = 'assets/product/mop.jpg'
+set image_url = 'assets/images/product/mop.jpg'
 where id = 18;
 
 update ecommerce.product
-set image_url = 'assets/product/mop_bucket.jpeg'
+set image_url = 'assets/images/product/mop_bucket.jpg'
 where id = 19;
 
 update ecommerce.product
-set image_url = 'assets/product/toilet_paper.jpg'
+set image_url = 'assets/images/product/toilet_paper.jpg'
 where id = 20;
 
 update ecommerce.product
-set image_url = 'assets/product/caution_sign.jpg'
+set image_url = 'assets/images/product/caution_sign.jpg'
 where id = 21;
 
 update ecommerce.product
-set image_url = 'assets/product/scrub_brush.jpg'
+set image_url = 'assets/images/product/scrub_brush.jpg'
 where id = 22;
 
 update ecommerce.product
-set image_url = 'assets/product/glass_cleaner.jpg'
+set image_url = 'assets/images/product/glass_cleaner.jpg'
 where id = 23;
  
 select * from ecommerce.product;
